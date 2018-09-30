b3dabd8eeed84e5453c2b04bd8b08f7c5b42fc10
Shinichi Masuda <masuda@my-style.jp>
2018-09-30 15:18:21 +0900

    実装完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180929051905) do
+ActiveRecord::Schema.define(version: 20180930051218) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
